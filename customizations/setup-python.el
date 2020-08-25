5c64c7e8e18fc79e9f59d4072841cb3d4c8f914b
Andrea Cadeddu <acadeddu@servercentral.com>
2020-08-25 08:28:03 -0500

    raises company type treshold as was a pita

--- customizations/setup-python.el
+++ customizations/setup-python.el
@@ -7,6 +7,6 @@
 (setq lsp-prefer-capf t )
 
 (add-hook 'after-init-hook 'global-company-mode)
-(setq company-idle-delay 0)
-(setq company-minimum-prefix-length 1)
+(setq company-idle-delay 0.3)
+(setq company-minimum-prefix-length 2)
 
